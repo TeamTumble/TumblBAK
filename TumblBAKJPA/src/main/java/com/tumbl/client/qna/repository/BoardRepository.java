d3d22cc35cc741a3fc6df09f051797d3e3ee4b41
poiurip <poiurip@DESKTOP-5H77CL7>
2018-06-22 13:42:20 +0900

    11

--- TumblBAKJPA/src/main/java/com/tumbl/client/qna/repository/BoardRepository.java
+++ TumblBAKJPA/src/main/java/com/tumbl/client/qna/repository/BoardRepository.java
@@ -3,11 +3,12 @@ package com.tumbl.client.qna.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.tumbl.client.qna.vo.QnaVO;
 
-public interface BoardRepository extends JpaRepository<QnaVO, Long>, PagingAndSortingRepository<QnaVO, Long> {
+public interface BoardRepository extends JpaRepository<QnaVO, Long>, PagingAndSortingRepository<QnaVO, Long>, JpaSpecificationExecutor<QnaVO> {
 	long findByQnum(long qnum);
 	Page<QnaVO> findByQnum(int qnum, Pageable pageable);
 	int countByQnum(int qnum);
